e7fc9adf2bb0bfc07aff3042cb1a5436a8658312
xianqiang <982535901@qq.com>
2019-11-27 15:33:32 +0800

    init

---
+++ src/main/java/com/bigdata/excutorReturnValue/ExcutorReturnValue.scala
@@ -0,0 +1,39 @@
+package com.bigdata.excutorReturnValue
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext, TaskContext}
+
+/**
+  * 我想要在driver端获取executor执行task返回的结果，
+  * 比如task是个规则引擎，我想知道每条规则命中了几条数据，请问这个怎么做呢？
+  */
+object ExcutorReturnValue {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("count")
+    val sc = new SparkContext(conf)
+    val rdd: RDD[String] = sc.parallelize(Array("1", "2", "3", "4", "5", "6")).repartition(4)
+    //
+    println(rdd.count())
+
+    val func = (itr: Iterator[String]) => {//Iterator是一个partition的数据集和
+      var count = 0
+      itr.foreach(each => {
+        count += 1
+      })
+      (TaskContext.getPartitionId(), count)
+    }
+
+    // Run a job on all partitions in an RDD and return the results in an array. 到driver端
+    val res: Array[(Int, Int)] = sc.runJob(rdd, func)
+
+    res.foreach(println)
+
+    sc.stop()
+
+  }
+
+}
