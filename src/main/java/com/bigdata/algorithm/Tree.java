8e0aaf87209682642ef453ec20262974fbe4015e
xianqiang <982535901@qq.com>
2020-03-30 13:49:51 +0800

    sort

---
+++ src/main/java/com/bigdata/algorithm/Tree.java
@@ -0,0 +1,25 @@
+package com.bigdata.algorithm;
+
+import javax.swing.tree.TreeNode;
+import java.util.List;
+
+public class Tree {
+
+    public static void main(String[] args) {
+
+//        前中后遍历
+//                前：根- 左-右
+//                中：左-根-右
+//                后：左-右-根
+//
+//        用递归遍历最简单
+
+    }
+
+//    private void preorderHelper(TreeNode root, List<Integer> result) {
+//        if (root == null) return;
+//        result.add(root.val); // 访问根节点
+//        preorderHelper(root.left, result); // 递归遍历左子树
+//        preorderHelper(root.right, result); //递归遍历右子树
+//    }
+}
